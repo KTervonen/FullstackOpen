f7e0c5cb683c26b5251a8ea385490ffa0bf7b6c0
Kasperi Tervonen <kasperi.tervonen@gmail.com>
2022-02-24 18:53:32 +0200

    add assignment 1.5
    merge course and parts into one JavaSript object and fix problems

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,17 +1,15 @@
-const Header = (pippeli) => {
-    console.log(pippeli)
-    return <h1>{pippeli.course}</h1>
+const Header = (props) => {
+    console.log(props.course.name)
+    return <h1>{props.course.name}</h1>
 }
 
 const Content = (props) => {
-    console.log(props.parts[0])
-    console.log(props.parts[1])
-    console.log(props.parts[2])
+    console.log(props.course.name)
     return (
         <div>
-            <Part part={props.parts[0]}/>
-            <Part part={props.parts[1]}/>
-            <Part part={props.parts[2]}/>
+            <Part part={props.course.parts[0]}/>
+            <Part part={props.course.parts[1]}/>
+            <Part part={props.course.parts[2]}/>
         </div>
     )
 }
@@ -20,7 +18,7 @@ const Content = (props) => {
 // sullotaan yhteen div-elementtiin, jonka Content-funktio palauttaa Appissa olevalle kutsulle.
 
 const Part = (props) => {
-    console.log(props.part)
+    console.log(props.part.name)
     return (
         <p>
             {props.part.name + ' ' + props.part.exercises}
@@ -30,38 +28,41 @@ const Part = (props) => {
 
 
 const Total = (props) => {
-    console.log(props.parts[0])
-    console.log(props.parts[1])
-    console.log(props.parts[2])
+    console.log(props.course.parts[0].name)
 
     return (
         <div>
-            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+            <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
         </div>
     )
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+    // Korjaa, mikä menee rikki!
+
     return (
         <div>
             <Header course={course}/>
-            <Content parts={parts}/>  {/* koitetaan ensin saada tämä toimimaan */}
-            <Total parts={parts}/>
+            <Content course={course}/>
+            <Total course={course}/>
         </div>
 
     )
